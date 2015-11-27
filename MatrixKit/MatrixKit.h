760dde44a88c7435a8c977f910dd8f7e49bf1c98
Antoine DAUGUET <dauguet.antoine@gmail.com>
2015-11-27 22:48:45 +0100

    Initial Commit

---
+++ MatrixKit/MatrixKit.h
@@ -0,0 +1,19 @@
+//
+//  MatrixKit.h
+//  MatrixKit
+//
+//  Created by Claire DAUGUET on 27/11/2015.
+//  Copyright © 2015 Antoine DAUGUET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MatrixKit.
+FOUNDATION_EXPORT double MatrixKitVersionNumber;
+
+//! Project version string for MatrixKit.
+FOUNDATION_EXPORT const unsigned char MatrixKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MatrixKit/PublicHeader.h>
+
+
